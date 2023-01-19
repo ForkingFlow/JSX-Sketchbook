3603553ccfe86f2ec6eef0fad08bbebdab362d7f
marcograhl <20419551+marcograhl@users.noreply.github.com>
2023-01-19 16:59:39 +0000

    waht is ha

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 const fs = require('node:fs');
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+plugins: [
+      react(),
+    ],
+
+  esbuild: {
+    loader: "jsx",
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        ".js": "jsx",
+        ".ts": "tsx",
+      },
+    },
+  },
   server: {
     port: 1987,
     host: true,
